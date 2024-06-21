33f6432843f0a536258d08552d1dcbb7fb7c2cb0
Vero060830 <sc21x2l@leeds.ac.uk>
2024-06-21 11:46:25 +0800

    Delete some comments

--- blueprints/api/data.py
+++ blueprints/api/data.py
@@ -38,35 +38,10 @@ def upload_file():
         filename = os.path.splitext(file.filename)[0] + '.wav'
         file_path = os.path.join(project_dir, filename)
         print('filename', file_path)
-        # # 将文件保存为临时文件
-        # temp_path = os.path.join(app.config['../../pytorch/dataset/my_music_file'], file.filename)
-        # file.save(temp_path)
-
-        # # 确保目录存在
-        # os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
         # 保存文件
         file.save(file_path)
 
-        # # # 使用 pydub 转换为 WAV 格式
-        # audio = AudioSegment.from_file(save_path)
-        # audio.export(save_path, format="wav")
-        #
-        #
-        # parser = argparse.ArgumentParser(description=__doc__)
-        # add_arg = functools.partial(add_arguments, argparser=parser)
-        # add_arg('configs', str, '../configs/bi_lstm.yml', '配置文件')
-        # add_arg('use_gpu', bool, True, '是否使用GPU预测')
-        # add_arg('audio_path', str, save_path, '音频路径')
-        # add_arg('model_path', str, '../models/BiLSTM_Emotion2Vec/best_model/', '导出的预测模型文件路径')
-        # args = parser.parse_args()
-        # print_arguments(args=args)
-        # print(77)
-        # print(infer.run(args))
-        #
-        # # 删除临时文件
-        # os.remove(temp_path)
-        # return f'File successfully uploaded and saved as {filename}',200
 
         parser = argparse.ArgumentParser(description=__doc__)
         add_arg = functools.partial(add_arguments, argparser=parser)
